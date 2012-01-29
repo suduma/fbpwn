3f3659a68038f7f4c5c522672e5303b9ec4ac5e3
HusseinElMotayam@gmail.com <HusseinElMotayam@gmail.com@b80e3924-a6f3-e8bc-780d-6ec52e04ccbb>
2012-01-29 22:52:26 +0000

    Added DictionaryBuilder module. Fixed issue 3

--- src/fbpwn/core/FacebookManager.java
+++ src/fbpwn/core/FacebookManager.java
@@ -20,17 +20,11 @@
  */
 package fbpwn.core;
 
-import com.gargoylesoftware.htmlunit.html.DomNodeList;
-import fbpwn.ui.FacebookGUI;
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
-import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
-import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
-import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+import com.gargoylesoftware.htmlunit.html.*;
+import fbpwn.plugins.core.DictionaryBuilderTask;
+import fbpwn.ui.FacebookGUI;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Constructor;
@@ -272,6 +266,7 @@ public class FacebookManager {
 	 * allClasses.add(DumpWallTask.class);
 	 * allClasses.add(ProfileClonerTask.class);
 	 */
+	allClasses.add(DictionaryBuilderTask.class);
 	try {
 	    Class<?> myclass = null;
 	    File Directory = new File(
